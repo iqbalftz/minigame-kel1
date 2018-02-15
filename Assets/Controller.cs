6617f9c3c49c06e5a62c39b5e95870248bd21248
M. Iqbal F <iqbal.ftz@gmail.com>
2018-02-16 06:28:38 +0700

    First commit

---
+++ Assets/Controller.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+	private float score;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position += new Vector3(Input.GetAxis("Horizontal"),0,0);
+	}
+
+	void OnCollisionEnter (Collision col)
+	{
+		if(col.gameObject.name == "Sphere(Clone)")
+		{
+			score++;
+			Destroy(col.gameObject);
+		}
+	}
+
+}
